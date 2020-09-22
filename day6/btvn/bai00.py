ba9438e2c4a75a8cdcc60228cc917c5be0e79839
Dung Nguyen <dungna@outlook.com>
2020-09-22 07:10:43 +0700

    BTVN day06

---
+++ day6/btvn/bai00.py
@@ -0,0 +1,11 @@
+
+my_dict = {
+    1: 3,
+    2: 4,
+    3: 6
+}
+tich = 1
+for i in my_dict:
+    tich *= my_dict[i]
+
+print("Tich cac phan tu: ", str(tich))
---
+++ day6/main.py
@@ -0,0 +1,23 @@
+# my_dict = {
+#     1: 'Mot',
+#     2: 'Hai',
+#     3: 'Ba',
+#     "hello": 2,
+# }
+#
+# print(my_dict)
+# print(my_dict[1])
+# print(my_dict["hello"])
+#
+# # my_dict.pop(3)
+# # print(my_dict)
+# my_dict.popitem()
+# print(my_dict)
+# print(my_dict.keys())
+# print(sorted(my_dict))
+
+dic = {}
+for i in range(10):
+    dic[i] = (i + 2)*2
+
+print(dic)
