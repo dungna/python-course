046a363f15d7134ec4aa54eac44b48c44d21995f
Dung Nguyen <dungnguyen@ladipage.com>
2020-09-22 16:11:57 +0700

    Add btvn

--- day6/btvn/bai05.py
+++ day6/btvn/bai05.py
@@ -1,1 +1,10 @@
 # Bài 05: Viết chương trình tạo ra dict với lớn hơn 3 phần tử, value của mỗi phần tử là một list có lớn hơn 5 phần tử. Truy cập và in ra phần tử thứ 5 trong phần value của mỗi phần tử trong dict
+
+d = {
+	"k1": (1, 2, 3, 4, 5),
+	"k2": ("hello", "chao", "add", "asadas", "thu5"),
+	"k3": ("e", "d", "c", "b", "a"),
+}
+
+for i in d:
+	print(d[i][4])
--- day6/btvn/bai07.py
+++ day6/btvn/bai07.py
@@ -1,8 +1,12 @@
-my_dict = {'a': 100, 'b':200, 'c':300}
-new_dict = {}
-keys = ['a','b']
+"""
+Bài 07: Viết chương trình tạo dict mới bằng cách trích xuất dữ liệu từ dict ban đầu theo tập các key cho trước
+Ví dụ:
+    dict ban đầu: sampleDict = {"name": "Kelly", "age":25, "salary": 8000, "city": "New york"}
+    keys = ["name", "salary"]
+    Output: {'name': 'Kelly', 'salary': 8000}
+"""
 
-for key in my_dict:
-    if key in keys:
-        new_dict.update({key: my_dict.get(key)})
+sampleDict = {"name": "Kelly", "age":25, "salary": 8000, "city": "New york"}
+keys = ["name", "salary"]
+new_dict = {i: sampleDict[i] for i in keys}
 print(new_dict)
